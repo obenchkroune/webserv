1c424efb901e30abdd772c7f7dd93f96bb42f3af
OUSSAMA BENCHKRON SAID <obenchkroune01@gmail.com>
2024-12-15 18:06:17 +0100

    feat(directory): Implement directory listing functionality with HTML template

--- srcs/server/Methods/GET.cpp
+++ srcs/server/Methods/GET.cpp
@@ -10,11 +10,12 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "DirectoryListing.hpp"
 #include "Server.hpp"
 #include "ServerClient.hpp"
 
-void ServerClient::ProcessGET(const Request &request, Response *response, bool send_data /* = true*/)
-{
+void ServerClient::
+    ProcessGET(const Request& request, Response* response, bool send_data /* = true*/) {
     std::vector<LocationConfig>::const_iterator file_location =
         ServerUtils::GetFileLocation(response->GetVirtualServer(), request.getUri());
     if (file_location == response->GetVirtualServer().locations.end())
@@ -22,38 +23,42 @@ void ServerClient::ProcessGET(const Request &request, Response *response, bool s
     std::pair<HttpStatus, std::string> file = ProcessFilePermission(request, file_location, R_OK);
     if (file.first.code != STATUS_OK)
         return SendErrorResponse(file.first, response);
-    std::string &file_name = file.second;
+    std::string& file_name = file.second;
 
     struct stat path_stat;
     stat(file_name.c_str(), &path_stat);
-    if (S_ISDIR(path_stat.st_mode))
-    {
+    if (S_ISDIR(path_stat.st_mode)) {
         std::vector<std::string>::const_iterator index_it = file_location->index.begin();
-        for (; index_it != file_location->index.end(); index_it++)
-        {
+        for (; index_it != file_location->index.end(); index_it++) {
             std::string index_file_name = file_name + '/' + *index_it;
-            if (access(index_file_name.c_str(), F_OK) == 0)
-            {
-                if (access(index_file_name.c_str(), R_OK) == 0)
-                {
+            if (access(index_file_name.c_str(), F_OK) == 0) {
+                if (access(index_file_name.c_str(), R_OK) == 0) {
                     file_name = index_file_name;
                     break;
                 }
-                return SendErrorResponse(HttpStatus(STATUS_FORBIDDEN, HTTP_STATUS_FORBIDDEN), response);
+                return SendErrorResponse(
+                    HttpStatus(STATUS_FORBIDDEN, HTTP_STATUS_FORBIDDEN), response
+                );
             }
         }
-        if (index_it == file_location->index.end())
-        {
-            if (file_location->autoindex)
-            {
-                std::cerr << "[Error] Directory listing is not yet implemented" << std::endl;
-                std::cerr << "Try specifying a file path like /index.html" << std::endl;
-                throw NotImplemented();
-                return SendErrorResponse(HttpStatus(STATUS_FORBIDDEN, HTTP_STATUS_FORBIDDEN), response);
-            }
-            else
-            {
-                return SendErrorResponse(HttpStatus(STATUS_FORBIDDEN, HTTP_STATUS_FORBIDDEN), response);
+        if (index_it == file_location->index.end()) {
+            if (file_location->autoindex) {
+                response->SetStatusHeaders(HTTP_STATUS_OK);
+                ResponseHeader header;
+                header.name  = "Content-Type";
+                header.value = "text/html";
+                response->AppendHeader(header);
+
+                std::string          html = DirectoryListing::generate(file_name, request.getUri());
+                std::vector<uint8_t> content(html.begin(), html.end());
+                response->AppendContent(content);
+                response->FinishResponse(true);
+                _server->QueueResponse(_socket_fd, response);
+                return;
+            } else {
+                return SendErrorResponse(
+                    HttpStatus(STATUS_FORBIDDEN, HTTP_STATUS_FORBIDDEN), response
+                );
             }
         }
         stat(file_name.c_str(), &path_stat);
@@ -62,11 +67,12 @@ void ServerClient::ProcessGET(const Request &request, Response *response, bool s
     size_t max_sz_limit = response->GetVirtualServer().max_body_size;
     if (file_location->max_body_size != response->GetVirtualServer().max_body_size)
         max_sz_limit = file_location->max_body_size;
-    if ((size_t)path_stat.st_size > max_sz_limit)
-    {
+    if ((size_t)path_stat.st_size > max_sz_limit) {
         std::cerr << "file too large: " << file_name << std::endl;
-        return SendErrorResponse(HttpStatus(STATUS_REQUEST_ENTITY_TOO_LARGE, HTTP_STATUS_REQUEST_ENTITY_TOO_LARGE),
-                                 response);
+        return SendErrorResponse(
+            HttpStatus(STATUS_REQUEST_ENTITY_TOO_LARGE, HTTP_STATUS_REQUEST_ENTITY_TOO_LARGE),
+            response
+        );
     }
 
     response->SetStatusHeaders(HTTP_STATUS_OK);
@@ -85,8 +91,7 @@ void ServerClient::ProcessGET(const Request &request, Response *response, bool s
     int file_fd = open(file_name.c_str(), O_RDONLY);
     if (file_fd < 0)
         throw ServerClientException("open() failed for file: " + file_name);
-    if (send_data == false)
-    {
+    if (send_data == false) {
         stat(file_name.c_str(), &path_stat);
         std::ostringstream content_length;
         content_length << path_stat.st_size;
@@ -94,16 +99,13 @@ void ServerClient::ProcessGET(const Request &request, Response *response, bool s
         header.value = content_length.str();
         response->AppendHeader(header);
         response->FinishResponse(false);
-    }
-    else
-    {
+    } else {
         response->ReadFile(file_fd);
         response->FinishResponse(true);
     }
     _server->QueueResponse(_socket_fd, response);
 }
 
-void ServerClient::ProcessHEAD(const Request &request, Response *response)
-{
+void ServerClient::ProcessHEAD(const Request& request, Response* response) {
     ProcessGET(request, response, false);
 }
